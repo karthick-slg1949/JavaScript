375b379dcbc56b9093e609828e9db81925f56dab
karthick-slg1949 <karthikarthick1949@gmail.com>
2024-06-11 11:32:37 +0530

    tst

---
+++ learn-st/Assignment_js/Array/array.js
@@ -0,0 +1,4 @@
+// 92. How to Create Arrays in JavaScript.
+
+let eid=[101,102,103,104]
+console.log(eid)
---
+++ learn-st/Assignment_js/object.js
@@ -0,0 +1,4 @@
+// how to print object in browser?
+
+let emp={"ename":"rahul"}
+document.write(JSON.stringify(emp))
