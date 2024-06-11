3497a3a845e8cbf61c2123f1d813244316160b3b
karthick-slg1949 <karthikarthick1949@gmail.com>
2024-06-11 12:00:00 +0530

    tst

---
+++ learn-st/Assignment_js/Array/object.js
@@ -0,0 +1,9 @@
+// 96. How to Create Array of Objects in JavaScript
+
+let employees=[{"id":101,"name":"karthick","sal":10000},
+    {"id":102,"name":"kumar","sal":20000},
+    {"id":103,"name":"Rahul","sal":30000}
+]
+for(employee of employees){
+console.log(employee)
+}
