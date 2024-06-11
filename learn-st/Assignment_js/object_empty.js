2a8c4ed7bf9fc48c7eb50566820dca6a682b2b9e
karthick-slg1949 <karthikarthick1949@gmail.com>
2024-06-11 11:16:46 +0530

    tst

---
+++ learn-st/Assignment_js/object_empty.js
@@ -0,0 +1,10 @@
+// How to verify given object is empty or not?
+
+let eids=[{"id":101,"Name":"Rahul","Sal":11000}]
+let ids={}
+if(ids.length>0){
+    console.log("Not Empty")
+}
+else{
+    console.log("Empty")
+}
