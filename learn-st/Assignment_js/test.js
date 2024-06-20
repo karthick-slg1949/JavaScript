ac62bda323ad67de03d7f3d26a531abf8006a310
karthick-slg1949 <karthikarthick1949@gmail.com>
2024-06-20 15:41:35 +0530

    tst

---
+++ learn-st/Assignment_js/test.js
@@ -0,0 +1,11 @@
+// class Test{
+//     constructor(){}//special method
+//     getDetails(){}//normal method
+// }
+// new Test()
+
+class Test{
+    constructor(){}  //specail method
+    getDetails(){}   //normal method
+ }
+ new Test()
