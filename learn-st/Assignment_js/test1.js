3a0cae9683d08c93c895ad43118d048028ecae27
karthick-slg1949 <karthikarthick1949@gmail.com>
2024-06-20 15:50:13 +0530

    tst

---
+++ learn-st/Assignment_js/test1.js
@@ -0,0 +1,10 @@
+class Test{
+    constructor(){
+        console.log("Constructor method-Test class")
+    }//special method
+    getDetails(){
+        console.log("GetDetalis method")
+    }//normal method
+}
+new Test(10,20,30)//how to innitialize object values?
+new Test()
