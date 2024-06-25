363a6e2caaeac3ab429cdad6c8dec08036f3c188
karthick-slg1949 <karthikarthick1949@gmail.com>
2024-06-25 19:41:50 +0530

    tst

---
+++ learn-st/Assignment_js/push.js
@@ -0,0 +1,8 @@
+let prices=[98,198,298,398]
+// create newprices variable by adding +1
+let new_prices=[]//empty array
+for(price of prices){
+    new_prices.push(price+1)
+}
+console.log(prices)
+console.log(new_prices)
