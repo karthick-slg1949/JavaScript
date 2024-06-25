0f799281744b966bbd905efa5a33770101c202ba
karthick-slg1949 <karthikarthick1949@gmail.com>
2024-06-25 19:45:32 +0530

    tst

--- learn-st/Assignment_js/each.js
+++ learn-st/Assignment_js/each.js
@@ -1,9 +1,9 @@
 let prices=[98,198,298,398]
 // create newprices variable by adding +1
-let new_prices=[]//empty array
-//using for of method
-for(price of prices){
-    new_prices.push(price+1)
-}
+
+//using for each method
+let new_prices=prices.forEach((price)=>{
+    return price+1
+})
 console.log(prices)
 console.log(new_prices)
